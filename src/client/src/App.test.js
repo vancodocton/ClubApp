5d7bfb3ae32e4255b9141891df032b1bb8708732
Nguyễn Kỳ Dương Trường <duongtruong.nguyenky@gmail.com>
2022-06-30 17:59:58 +0700

    Update react client from template of dotnet new react "ClientApp folder"

--- src/client/src/App.test.js
+++ src/client/src/App.test.js
@@ -1,8 +1,14 @@
-import { render, screen } from '@testing-library/react';
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { MemoryRouter } from 'react-router-dom';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+it('renders without crashing', async () => {
+  const div = document.createElement('div');
+  const root = createRoot(div);
+  root.render(
+    <MemoryRouter>
+      <App />
+    </MemoryRouter>);
+  await new Promise(resolve => setTimeout(resolve, 1000));
 });
